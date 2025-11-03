89c9bee1651cc36c770ea4dab5f6fe89874d8274
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-03 21:32:16 +0000

    can we space the text please (_for element h2_)

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -100,7 +100,7 @@ export default function ServicesPage() {
                     <div className="container px-4 md:px-6">
                         <div className="flex flex-col items-center justify-center space-y-4 text-center">
                             <div className="space-y-2">
-                                <h2 className="text-3xl font-headline font-bold tracking-tighter text-primary sm:text-5xl">Your Coaching Journey</h2>
+                                <h2 className="text-3xl font-headline font-bold tracking-wide text-primary sm:text-5xl">Your Coaching Journey</h2>
                                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                                 My coaching process is designed to be clear, supportive, and transformative.
                                     <br />
