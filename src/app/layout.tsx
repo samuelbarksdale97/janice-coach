50e8a8b793fcc11c1516cac06613e86d46355180
Samuel Barksdale <samuel.barksdale97@gmail.com>
2025-11-14 13:24:18 -0500

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { cn } from '@/lib/utils';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Taylor Leadership Coaching',
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
