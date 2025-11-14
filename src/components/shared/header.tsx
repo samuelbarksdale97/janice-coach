a0ed76ea87cceda6fe77c6522e6ae1e8a96d5413
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-14 15:42:48 +0000

    please make this bigger on mobile like 1080x1080 if possible (_for eleme

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -86,7 +86,13 @@ export function Header() {
             <DialogContent className="mobile-menu-overlay p-0">
                 <div className="flex h-24 items-center justify-between px-4 border-b">
                   <Link href="/" onClick={() => setIsOpen(false)} className="flex items-center space-x-2">
-                    
+                    <Image 
+                        src="https://firebasestorage.googleapis.com/v0/b/studio-7158004547-ae16d.firebasestorage.app/o/TYL-FinalLogo_BLU.png?alt=media&token=48d51385-c610-491c-9523-b9e5fcc42edd"
+                        alt="Taylor Leadership Coaching Logo"
+                        width={320}
+                        height={80}
+                        className="w-auto h-16"
+                    />
                   </Link>
                   <DialogClose asChild>
                     <button className="p-2">
