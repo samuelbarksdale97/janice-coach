a93c038a0016019c648460432e5fc31b4629e1c4
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-16 15:26:22 +0000

    Can you fix the layout shift that happens upon load for the section abov

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -87,7 +87,7 @@ export default function AboutPage() {
                 </section>
 
                 <div className="container mx-auto px-4 lg:hidden animate-glide-up">
-                    <div className="flex justify-center -my-8">
+                    <div className="flex justify-center py-8">
                         <Image
                         src="https://firebasestorage.googleapis.com/v0/b/studio-7158004547-ae16d.firebasestorage.app/o/TYL-FinalLogo_GLD.png?alt=media&token=edf6b10c-4dca-41b8-8e62-9ed961262133"
                         alt="Taylor Leadership Coaching Gold Logo"
