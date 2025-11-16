43ed45b7fc4bb91478e86282f68c233741b603eb
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-16 09:12:16 +0000

    can we move pic under above section to reduce toppadding?

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -85,8 +85,8 @@ export default function AboutPage() {
                         </Card>
                     </div>
                 </section>
-                <section id="logo-display" className="bg-background lg:hidden">
-                    <div className="container mx-auto px-4">
+                <section className="w-full py-8 md:py-12 bg-background relative z-10">
+                    <div className="container mx-auto px-4 lg:hidden">
                         <div className="flex justify-center">
                             <Image
                             src="https://firebasestorage.googleapis.com/v0/b/studio-7158004547-ae16d.firebasestorage.app/o/TYL-FinalLogo_GLD.png?alt=media&token=edf6b10c-4dca-41b8-8e62-9ed961262133"
@@ -97,8 +97,6 @@ export default function AboutPage() {
                             />
                         </div>
                     </div>
-                </section>
-                <section className="w-full py-8 md:py-12 bg-background relative z-10">
                     <div className="container px-4 md:px-6">
                         <h2 className="text-3xl font-headline font-bold text-primary text-center mb-8">More About Me</h2>
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
