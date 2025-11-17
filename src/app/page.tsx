5b9f907d8d18a7e0d443c8f622f07ae809846402
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-17 13:49:14 +0000

    Can we make this logo on the homepage one size bigger on medium and larg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
           <section id="logo-display" className="bg-background py-8 md:py-12">
             <div className="container mx-auto px-4">
               <div className="flex justify-center animate-glide-up">
-                <div className="w-full max-w-2xl h-64">
+                <div className="w-full max-w-2xl h-64 md:h-72">
                   <Image
                     src="https://firebasestorage.googleapis.com/v0/b/studio-7158004547-ae16d.firebasestorage.app/o/TYL-FinalLogo_HRZ%20OG.png?alt=media&token=16336bba-a715-4140-8631-4067db3359ad"
                     alt="Taylor Leadership Coaching Horizontal Logo"
@@ -205,6 +205,8 @@ export default function Home() {
     </>
   );
 }
+
+    
 
     
 
