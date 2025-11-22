857a025463d9a0a0f996d0c0ff105ea52d2dee4c
Tysha Barksdale <tysha.barksdale@gmail.com>
2025-11-22 02:05:13 +0000

    Can we space out a little more its still overlapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
                     <div className="relative z-10 p-4 max-w-4xl mx-auto animate-glide-up flex flex-col items-center">
                         
                         <h1 className="text-3xl md:text-5xl font-headline font-bold tracking-normal text-shadow-lg mt-8">
-                            What becomes possible when you stop performing, start leading and just be <span className="italic text-secondary">yourself</span><span className="ml-1">?</span>
+                            What becomes possible when you stop performing, start leading and just be <span className="italic text-secondary">yourself</span><span className="ml-2">?</span>
                         </h1>
                         
                         <div className="mt-12 flex flex-col sm:flex-row items-center justify-center gap-4">
